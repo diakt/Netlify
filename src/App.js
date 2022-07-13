62e3d4f0f7830f67077d6a96bf75169088979205
diakt <igs2107@columbia.edu>
2022-07-13 15:17:12 -0400

    all

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
-					Edit <code>src/App.js</code> and save to reload.
+					DUBGI <code>src/App.js</code> and save to reload.
 				</p>
 				<a
 					className='App-link'
